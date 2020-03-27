6fbe5fe477d0636c8b04ea920a95acca79767829
Abhijay Saini <abhijay.saini@gmail.com>
2020-03-27 16:16:58 -0500

    sigmoid function added

--- Network.java
+++ Network.java
@@ -4,5 +4,12 @@ import javax.imageio.ImageIO;
 class Network {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(sigmoid(6));
+    
+    }
+
+    
+    public static double sigmoid(double value) {
+        return 1/(1 + Math.exp(-value));
     }
 }
