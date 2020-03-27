8ec920e57fdb31bbb9c97c28371b7d6c9e559195
blasteroid297 <blasteroid297@gmail.com>
2020-03-27 16:30:05 -0500

    Update Network.java

--- Network.java
+++ Network.java
@@ -5,18 +5,12 @@ import java.io.IOException;
 
 class Network {
     public static void main(String[] args) throws IOException{
+        
         File file = new File("eight.jpg");
         BufferedImage img = ImageIO.read(file);
         int width = img.getWidth();
         int height = img.getHeight();
         System.out.println(width + " " + height);
-        System.out.println( (img.getRGB(0, 0)>>8) & 0xff);
-        /*
-            (p >> 8) & 0xff - greed
-            (p>>16) & 0xff - red
-            (p) & 0xff - blue
-            ( (0.3 * R) + (0.59 * G) + (0.11 * B) ).
-        */
     }
 
     /*
@@ -25,5 +19,9 @@ class Network {
      */
     public static double sigmoid(double value) {
         return 1/(1 + Math.exp(-value));
+    }
+
+    public static double greyscale(){
+        return 0;
     }
 }
