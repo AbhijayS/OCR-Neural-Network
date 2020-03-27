fc91c6ec431d8db814c4c5c024a81e72b87c2b7c
Abhijay Saini <abhijay.saini@gmail.com>
2020-03-27 16:29:03 -0500

    rgb comments

--- Network.java
+++ Network.java
@@ -5,12 +5,18 @@ import java.io.IOException;
 
 class Network {
     public static void main(String[] args) throws IOException{
-        
         File file = new File("eight.jpg");
         BufferedImage img = ImageIO.read(file);
         int width = img.getWidth();
         int height = img.getHeight();
         System.out.println(width + " " + height);
+        System.out.println( (img.getRGB(0, 0)>>8) & 0xff);
+        /*
+            (p >> 8) & 0xff - greed
+            (p>>16) & 0xff - red
+            (p) & 0xff - blue
+            ( (0.3 * R) + (0.59 * G) + (0.11 * B) ).
+        */
     }
 
     /*
