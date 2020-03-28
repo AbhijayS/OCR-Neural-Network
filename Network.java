c227c9d1d46f70899b354bb4bbaa1688e152377b
blasteroid297 <blasteroid297@gmail.com>
2020-03-28 14:52:45 -0500

    Update Network.java

--- Network.java
+++ Network.java
@@ -11,18 +11,7 @@ class Network {
         int width = img.getWidth();
         int height = img.getHeight();
         System.out.println(width + " " + height);
-<<<<<<< HEAD
-=======
         System.out.println( (img.getRGB(0, 0)>>8) & 0xff);
-        /*
-            (p >> 8) & 0xff - green
-            (p>>16) & 0xff - red
-            (p) & 0xff - blue
-            ( (0.3 * R) + (0.59 * G) + (0.11 * B) ).
-        */
-
->>>>>>> 690b1c223ddcfa937b285dc606ff575b1e3279bf
-        
     }
 
     /*
@@ -33,7 +22,19 @@ class Network {
         return 1/(1 + Math.exp(-value));
     }
 
-    public static double greyscale(){
+    public static double grayscale(int p){
+        
+        int alpha = (p >> 24) & 0xff;
+        int red = (p >> 16) & 0xff;
+        int green = (p >> 8) & 0xff;
+        int blue = (p) & 0xff;
+        System.out.println("argb: " + alpha + ", " + red + ", " + green + ", " + blue);
         return 0;
+        /*
+            (p >> 8) & 0xff - green
+            (p>>16) & 0xff - red
+            (p) & 0xff - blue
+            ( (0.3 * R) + (0.59 * G) + (0.11 * B) ).
+        */ 
     }
 }
