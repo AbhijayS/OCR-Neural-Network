65617950c3d66a43c9c5162fd3068f5cc7dc870c
Abhijay Saini <abhijay.saini@gmail.com>
2020-03-28 14:49:16 -0500

    added comments

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -12,12 +12,6 @@ class NeuralNetwork {
         int height = img.getHeight();
         System.out.println(width + " " + height);
         System.out.println( (img.getRGB(0, 0)>>8) & 0xff);
-        /*
-            (p >> 8) & 0xff - green
-            (p>>16) & 0xff - red
-            (p) & 0xff - blue
-            ( (0.3 * R) + (0.59 * G) + (0.11 * B) ).
-        */
 
         // System.out.println(scale(20, 0, 255, 0, 1));
     }
@@ -73,12 +67,21 @@ class NeuralNetwork {
         return 1/(1 + Math.exp(-value));
     }
 
+    /*
+     * @return a value between 0 and 1 representing the brightness of a pixel
+     * (p >> 8) & 0xff - green
+     * (p>>16) & 0xff - red
+     * (p) & 0xff - blue
+     * ( (0.3 * R) + (0.59 * G) + (0.11 * B) )
+     */
     private double greyscale() {
         return 0;
     }
 
     /*
      * Scales a value in one range to another
+     * 0 - 255
+     * 0 - 1
      */
     private double scale(double value, double x1, double x2, double y1, double y2) {
         double ratio = ((value-x1) / (x2-x1));
