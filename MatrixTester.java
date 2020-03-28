42e8ba1a2bb548d5c6c8a092f93a0b80208f14e7
Abhijay Saini <abhijay.saini@gmail.com>
2020-03-28 15:59:30 -0500

    matrix library tested

--- MatrixTester.java
+++ MatrixTester.java
@@ -1,7 +1,31 @@
 import org.ejml.data.Matrix;
+import org.ejml.simple.SimpleMatrix;
 
 class MatrixTester {
     public static void main(String[] args) {
-        
+        /*
+        2 4  5
+        1 3  6
+
+        2*5 + 4*6 = 34
+        1*5 + 3*6 = 23
+         */
+
+        float[][] data1 = {
+            {2, 4},
+            {1, 3}
+        };
+
+        float[][] data2 = {
+            {5},
+            {6}
+        };
+
+        SimpleMatrix m1 = new SimpleMatrix(data1);
+        SimpleMatrix m2 = new SimpleMatrix(data2);
+
+        SimpleMatrix result = m1.mult(m2);
+
+        System.out.println(result);
     }
 }
